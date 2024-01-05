78320b811fc518b25c3a089ce3d6b21e7d3fd353
Rafael <rafik.pilumyan@gmail.com>
2024-01-05 16:22:58 +0200

    Update: RestaurantDetails about

--- components/restaurantDetails.tsx
+++ components/restaurantDetails.tsx
@@ -2,7 +2,7 @@ import { View, Text, Image, TouchableOpacity } from 'react-native';
 import React, { useLayoutEffect, useState } from 'react';
 import ParallaxScrollView from '../components/ParallaxScrollView.js';
 import { useNavigation } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, FontAwesome5, AntDesign } from '@expo/vector-icons';
 
 const RestaurantDetails = ({ post }) => {
   const navigation = useNavigation();
@@ -52,15 +52,35 @@ const RestaurantDetails = ({ post }) => {
           <Image className="w-full h-full" source={{ uri: post.profileImage }} resizeMode="cover" />
         )}
         stickyHeaderHeight={80}
-        contentBackgroundColor="white"
+        contentBackgroundColor="#d3d3d3"
         renderStickyHeader={() => (
           <View className={styles.headerContainer}>
             <Text className={styles.headerText}>{post.name}</Text>
           </View>
         )}
         scrollEvent={handleScroll}>
-        <View className={styles.container}>
-          <Text>Details</Text>
+        <View className={styles.namesContainer}>
+          <View className="m-6">
+            <Text className={styles.restaurantName}>{post.name}</Text>
+            <View className={styles.deliveryTextsContainer}>
+              <Ionicons name="bicycle" size={18} color="black" />
+              <Text className={styles.deliveryTexts}>Delivery</Text>
+              <Text>・</Text>
+              <FontAwesome5 name="walking" size={15} color="black" />
+              <Text className={styles.deliveryTexts}>Pickup</Text>
+              <Text>・</Text>
+
+              <Text className={styles.delyveryTextMoreInfo}>More info</Text>
+              <AntDesign
+                name="right"
+                size={14}
+                color="black"
+                className={styles.delyveryTextMoreInfo}
+              />
+            </View>
+            <View className="h-[0.5px] bg-slate-300 my-4" />
+            <Text className={styles.deliveryTexts}>{post.about}</Text>
+          </View>
         </View>
       </ParallaxScrollView>
     </>
@@ -68,11 +88,15 @@ const RestaurantDetails = ({ post }) => {
 };
 
 const styles = {
-  container: 'bg-white rounded-full',
+  namesContainer: 'flex rounded-2xl -mt-12 bg-white',
   roundButton: 'w-10 h-10 bg-transparent rounded-full justify-center items-center',
   rightContainer: 'flex flex-row justify-center items-center gap-2',
   headerContainer: 'justify-end ml-28 h-16',
   headerText: 'text-xl font-bold',
+  restaurantName: 'text-2xl font-bold',
+  deliveryTextsContainer: 'flex flex-row items-center',
+  deliveryTexts: 'text-sm ml-1 text-gray-700',
+  delyveryTextMoreInfo: 'text-sm font-bold',
 };
 
 export default RestaurantDetails;
