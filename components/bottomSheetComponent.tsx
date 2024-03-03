22ed7e2085f24cec20189b84765331450338ca82
Rafael <rafik.pilumyan@gmail.com>
2024-03-03 19:15:27 +0200

    Update: bottom sheet progress timer

--- components/bottomSheetComponent.tsx
+++ components/bottomSheetComponent.tsx
@@ -1,23 +1,64 @@
+import React, { useMemo, useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
-import React, { useMemo } from 'react';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { AntDesign } from '@expo/vector-icons';
 
+const trackData = [
+  {
+    id: '0',
+    text: 'has delayed your order by 15 min',
+  },
+  {
+    id: '1',
+    text: 'Courier has been assigned',
+  },
+  {
+    id: '2',
+    text: 'Preparing your order',
+  },
+  {
+    id: '3',
+    text: 'Courier is on its way',
+  },
+  {
+    id: '4',
+    text: 'Courier is delivering your order',
+  },
+];
+
 const BottomSheetComponent = () => {
   const snapPoints = useMemo(() => ['20%', '100%'], []);
+  const [progress, setProgress] = useState(0); // Progress of the bar
 
-  const now = new Date();
-  const futureTime = new Date(now.getTime() + 15 * 60000); // Adding 15 minutes (15 * 60000 milliseconds)
-  const formattedTime = futureTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  // Get current time and add 15 minutes
+  const deliveryTime = useMemo(() => {
+    const currentTime = new Date();
+    currentTime.setMinutes(currentTime.getMinutes() + 15);
+    return currentTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit' });
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgress((prevProgress) => {
+        if (prevProgress < 1) {
+          return prevProgress + 1 / 80; // Update progress every second
+        } else {
+          clearInterval(interval);
+          return 1;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <>
-      {/* Bottom Sheet */}
       <BottomSheet index={0} snapPoints={snapPoints}>
-        <View className="flex flex-1 px-4 mt-2">
+        <View className="flex flex-1 px-6 mt-2">
           {/* 1 row Time */}
           <View className="flex flex-row justify-between items-center">
-            <Text className="text-5xl text-black">{formattedTime}</Text>
+            <Text className="text-5xl text-black">{deliveryTime}</Text>
             <View className="flex items-end">
               <Text className="text-gray-500 text-base">Estimated time of</Text>
               <Text className="text-gray-500 text-base">delivery</Text>
@@ -31,54 +72,41 @@ const BottomSheetComponent = () => {
           <View className="flex flex-row h-72 justify-start">
             {/* Progress Time */}
             <View className="flex flex-col">
-              <Text className="mr-4 text-base text-gray-500">15:25</Text>
-              <Text className="mr-4 mt-11 text-base text-gray-500">15:25</Text>
-              <Text className="mr-4 mt-11 text-base text-gray-500">15:25</Text>
-              <Text className="mr-4 mt-10 text-base text-gray-500">15:25</Text>
-              <Text className="mr-4 mt-10 text-base text-gray-500">15:25</Text>
+              {trackData.map((track, index) => (
+                <Text
+                  key={track.id}
+                  className={`mr-4 text-base text-gray-500 ${
+                    index <= Math.floor(progress * 4) ? 'text-gray-500' : 'hidden'
+                  }`}>
+                  {index <= Math.floor(progress * 4) ? deliveryTime : null}
+                </Text>
+              ))}
             </View>
 
             {/* Progress Line */}
             <View className="flex flex-col border-[1px] border-[#34BB78] ">
-              {/* 1st buble */}
-              <View className="w-6 h-6 absolute bg-[#34BB78] rounded-full right-[-12px] top-[-3px] items-center">
-                <AntDesign name="check" size={16} color="white" style={{ marginTop: 4 }} />
-              </View>
-              {/* 2nd buble */}
-              <View className="w-6 h-6 absolute bg-[#34BB78] rounded-full right-[-12px] top-[67px] items-center">
-                <AntDesign name="check" size={16} color="white" style={{ marginTop: 4 }} />
-              </View>
-              {/* 3nd buble */}
-              <View className="w-6 h-6 absolute bg-[#34BB78] rounded-full right-[-12px] top-[135px] items-center">
-                <AntDesign name="check" size={16} color="white" style={{ marginTop: 4 }} />
-              </View>
-              {/* 4nd buble */}
-              <View className="w-6 h-6 absolute bg-[#34BB78] rounded-full right-[-12px] top-[200px] items-center">
-                <AntDesign name="check" size={16} color="white" style={{ marginTop: 4 }} />
-              </View>
-              {/* 5nd buble */}
-              <View className="w-6 h-6 absolute bg-[#34BB78] rounded-full right-[-12px] top-[265px] items-center">
-                <AntDesign name="check" size={16} color="white" style={{ marginTop: 4 }} />
-              </View>
+              {trackData.map((track, index) => (
+                <View
+                  key={track.id}
+                  className={`w-6 h-6 absolute bg-[#34BB78] rounded-full right-[-12px] ${
+                    index <= Math.floor(progress * 4) ? 'top-[-3px]' : 'top-[265px]'
+                  } items-center`}>
+                  {index <= Math.floor(progress * 4) && (
+                    <AntDesign name="check" size={16} color="white" style={{ marginTop: 4 }} />
+                  )}
+                </View>
+              ))}
             </View>
 
             {/* Progress Text */}
             <View className="flex flex-col">
-              <Text className="ml-6 text-base text-gray-500">
-                has deployd your order by 15 min.
-              </Text>
-              <Text className="ml-6 mt-11 text-base text-gray-500">
-                has deployd your order by 15 min.
-              </Text>
-              <Text className="ml-6 mt-11 text-base text-gray-500">
-                has deployd your order by 15 min.
-              </Text>
-              <Text className="ml-6 mt-10 text-base text-gray-500">
-                has deployd your order by 15 min.
-              </Text>
-              <Text className="ml-6 mt-10 text-base text-gray-500">
-                has deployd your order by 15 min.
-              </Text>
+              {trackData.map((track, index) => (
+                <Text
+                  key={track.id}
+                  className="ml-6 text-base text-gray-500">
+                  {track.text}
+                </Text>
+              ))}
             </View>
           </View>
         </View>
